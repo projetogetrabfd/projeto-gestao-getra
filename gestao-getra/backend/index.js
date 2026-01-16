aa40b8140ed84571238581cdcea2c51c1bc46ff4
MilitaoAraujo <militaoaraujorego@gmail.com>
2026-01-16 09:46:43 -0300

    Add files via upload

--- gestao-getra/backend/index.js
+++ gestao-getra/backend/index.js
@@ -1,7 +1,7 @@
-// ARQUIVO: backend/index.js
+
 const express = require('express');
 const cors = require('cors'); // 1. Importar o CORS
-const routes = require('./src/routes'); // Suas rotas
+const routes = require('./src/routes');
 
 const app = express();
 app.use(cors()); 
