f2f766ef72d00b2f1269d3657a680bfb3381417d
MilitaoAraujo <militaoaraujorego@gmail.com>
2026-01-16 09:48:58 -0300

    Add files via upload

--- gestao-getra/backend/src/middlewares/mockAuth.js
+++ gestao-getra/backend/src/middlewares/mockAuth.js
@@ -1,7 +1,7 @@
 const ROLES = require("../config/roles");
 
 function mockAuth(req, res, next) {
-  // Para a demonstração, vamos ler a 'role' e o 'id' que o Frontend envia nos headers
+  // Para a demonstração, vai ler a 'role' e o 'id' que o Frontend envia nos headers
   // Se não for enviado nada, ele assume CLIENTE por padrão para segurança
   const userRole = req.headers['x-user-role'] || ROLES.CLIENTE;
   const userId = req.headers['x-user-id'] || 99;
